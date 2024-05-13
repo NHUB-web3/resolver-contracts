8e957bd4ad4030aee4eb1f560f57192153e1219c
Feka7 <feka7@hotmail.it>
2024-05-13 22:33:22 +0200

    add get allowlist members function

--- contracts/resolvers/AttesterAllowList/AttesterAllowlistResolver.sol
+++ contracts/resolvers/AttesterAllowList/AttesterAllowlistResolver.sol
@@ -10,22 +10,32 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @title AttesterAllowlistResolver
 /// @notice This contract allows an owner to manage an allowlist of attesters
 contract AttesterAllowlistResolver is SchemaResolver, Ownable {
-
     using EnumerableSet for EnumerableSet.AddressSet;
 
     EnumerableSet.AddressSet private _allowlist;
 
     event AddedToAllowlist(address indexed addedBy, address indexed attester);
-    event RemovedFromAllowlist(address indexed removedBy, address indexed attester);
-    
-    constructor(IEAS eas, address initialOwner) SchemaResolver(eas) Ownable(initialOwner) {
-    }
+    event RemovedFromAllowlist(
+        address indexed removedBy,
+        address indexed attester
+    );
 
-    function onAttest(Attestation calldata attestation, uint256 /*value*/) internal view override returns (bool) {
+    constructor(IEAS eas, address initialOwner)
+        SchemaResolver(eas)
+        Ownable(initialOwner)
+    {}
+
+    function onAttest(
+        Attestation calldata attestation,
+        uint256 /*value*/
+    ) internal view override returns (bool) {
         return _allowlist.contains(attestation.attester);
     }
 
-    function onRevoke(Attestation calldata /*attestation*/, uint256 /*value*/) internal pure override returns (bool) {
+    function onRevoke(
+        Attestation calldata, /*attestation*/
+        uint256 /*value*/
+    ) internal pure override returns (bool) {
         return true;
     }
 
@@ -47,4 +57,11 @@ contract AttesterAllowlistResolver is SchemaResolver, Ownable {
         return _allowlist.contains(attester);
     }
 
+    function getAllowlist() public view returns (address[] memory) {
+        address[] memory allowlist = new address[](_allowlist.length());
+        for (uint256 i = 0; i < _allowlist.length(); i++) {
+            allowlist[i] = _allowlist.at(i);
+        }
+        return allowlist;
+    }
 }
